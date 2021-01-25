d196d011876e6c43ce428924aa0eab3ffc328067
Learde <leardeleo@gmail.com>
2021-01-26 00:34:43 +0300

    Style application

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,14 +1,15 @@
 import './Task.scss';
+import 'checked.svg';
 import React from 'react';
 
 const Task = (props) => {
 	let classList = 'Task ' + props.className;
 	if (props.done) {
-		classList += 's';
+		classList += ' Task--checked';
 	}
 	return (
 		<div className={classList}>
-			<div className="Task__checkbox" onClick={props.click.bind(this, props.id)}>[]</div>
+			<div className="Task__checkbox" onClick={props.click.bind(this, props.id)}></div>
 			<p className="Task__text">{props.text}</p>
 		</div>
 	);
